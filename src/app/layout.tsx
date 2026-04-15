87a8673b672e5b750bc757ae5254cfb56c40c5ed
KovacsGroup <kovacs_fx@proton.me>
2026-04-15 12:15:33 +0200

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { JetBrains_Mono, Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geistHeading = Geist({subsets:['latin'],variable:'--font-heading'});
+
+const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-mono", jetbrainsMono.variable, geistHeading.variable)}>
       <body>{children}</body>
     </html>
   );
